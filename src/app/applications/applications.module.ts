5ce124cf24cf5a1bab89536e98bb0d37c6af7b2c
karan <karan.g@codezen.in>
2022-01-10 01:50:15 +0530

    Application Module UI Added of Stages

--- src/app/applications/applications.module.ts
+++ src/app/applications/applications.module.ts
@@ -11,13 +11,35 @@ import { VerificationComponent } from './verification/verification.component';
 import { ENachSigningComponent } from './e-nach-signing/e-nach-signing.component';
 import { DisbursementComponent } from './disbursement/disbursement.component';
 import { StageTriggersComponent } from './stage-triggers/stage-triggers.component';
-
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @NgModule({
   declarations: [FormFillingComponent, DocumentUploadComponent, UnderwritingComponent, OfferProposedComponent, OfferAcceptanceComponent, VerificationComponent, ENachSigningComponent, DisbursementComponent, StageTriggersComponent],
   imports: [
     CommonModule,
-    ApplicationsRoutingModule
+    ApplicationsRoutingModule,
+    NzPageHeaderModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NzButtonModule,
+    NzTableModule,
+    NzCardModule,
+    NzInputModule,
+    NzSelectModule,
+    NzUploadModule,
+    NzIconModule,
+    NzDropDownModule,
+    NzMenuModule
   ]
 })
 export class ApplicationsModule { }
