694d4b7a284849038d9f857a51df1dc49e20469c
Ajmal <ajmal.m@codezen.in>
2022-02-07 16:45:25 +0530

    In Progress

--- src/app/users/payout-structure/payout-structure.module.ts
+++ src/app/users/payout-structure/payout-structure.module.ts
@@ -2,6 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PayoutStructureComponent } from './payout-structure/payout-structure.component';
 import { PayoutStructureRoutingModule } from './payoutStructure-routing.modules';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
@@ -9,7 +26,26 @@ import { PayoutStructureRoutingModule } from './payoutStructure-routing.modules'
   declarations: [PayoutStructureComponent],
   imports: [
     CommonModule,
-    PayoutStructureRoutingModule
+    PayoutStructureRoutingModule,
+    NzRadioModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NzCardModule,
+    NzTableModule,
+    NzIconModule,
+    NzSelectModule,
+    NzInputModule,
+    NzButtonModule,
+    NzTagModule,
+    NzDropDownModule,
+    NzPageHeaderModule,
+    NzModalModule,
+    NzFormModule,
+    NzDividerModule,
+    NzUploadModule,
+    NzCheckboxModule,
+    SharedModule
+
   ]
 })
 export class PayoutStructureModule { }
