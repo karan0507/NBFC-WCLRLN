752a1d3b8fe5971c68bbb17acca0f27dad5ec223
karan <karan.g@codezen.in>
2022-02-10 03:35:14 +0530

    Loan Application APIsAccept Reject Offer added

--- src/app/applications/form-filling/form-filling.component.ts
+++ src/app/applications/form-filling/form-filling.component.ts
@@ -4,117 +4,149 @@ import { differenceInCalendarDays } from 'date-fns';
 import { HttpService } from 'src/app/services/http.service';
 
 @Component({
-  selector: 'app-form-filling',
-  templateUrl: './form-filling.component.html',
-  styleUrls: ['./form-filling.component.css']
+      selector: 'app-form-filling',
+      templateUrl: './form-filling.component.html',
+      styleUrls: ['./form-filling.component.css']
 })
 export class FormFillingComponent implements OnInit {
-  checked :boolean = false;
-  indeterminate : boolean = false;
-  listOfCurrentPageData: readonly Data[] = [];
-  setOfCheckedId = new Set<number>();
-  loanApplicationData : any = [];
-  total_count:any;
-  _currentDate:any;
-  _currentId :any;
-  console = console;
-  _activeLoans: any = [];
-  today = new Date();
-  api_calling_loader: boolean;
-  disabledDate = (current: Date): boolean => {
-        // Can not select days before today and today
-        return differenceInCalendarDays(current, this.today) > 0;
+      checked: boolean = false;
+      filters: any;
+      productFilters: any;
+      indeterminate: boolean = false;
+      listOfCurrentPageData: readonly Data[] = [];
+      setOfCheckedId = new Set<number>();
+      loanApplicationData: any = [];
+      total_count: any;
+      _currentDate: any;
+      _currentId: any;
+      console = console;
+      _checkedLoanList: any[];
+      _activeLoans: any = [];
+      today = new Date();
+      api_calling_loader: boolean;
+      stageMasterList: any;
+      _currentStageStatus: any;
+      disabledDate = (current: Date): boolean => {
+            // Can not select days before today and today
+            return differenceInCalendarDays(current, this.today) > 0;
       };
 
-  // Modal Boolean Values
-  _isUpdateStatus: boolean = false;
-  statusList : any;
-  constructor(public https: HttpService) { }
+      // Modal Boolean Values
+      _isUpdateStatus: boolean = false;
+      statusList: any;
+      constructor(public https: HttpService) { }
 
-  ngOnInit(): void {
-    this.getFormLoanData();
-  }
-
-
-  getFormLoanData(id?) {
-    this.api_calling_loader = true
-    var data = {'datapoint':'loan_application', 'endpoint':'LoanApplication?stage_id=1', 'source':'Onboarding'}
-    this.https.fetchLoanApplicationList(data).subscribe(res => {
-      this.loanApplicationData = res?.data?.results;
-      this.total_count = res?.data?.total_count;
-      this.api_calling_loader = false
-    },(err)=>{
-      this.api_calling_loader = false
-    })
-  }
+      ngOnInit(): void {
+            this.getFormLoanData();
+      }
 
 
-  getIdWiseData(id?, index?){
-   let data = {'datapoint':'loan_application', 'endpoint':'LoanApplication?id='+ id, 'source':'Onboarding'};
-   this.https.fetchLoanApplicationList(data).subscribe(res=> {
-     this._activeLoans.push(res?.data?.results[0]);
-     this.loanApplicationData[index].expanddata = res?.data?.results[0];
-     console.log(this.loanApplicationData[index].expanddata)
-   })
-  }
+      getFormLoanData(id?) {
+            this.api_calling_loader = true
+            var data = { 'datapoint': 'loan_application', 'endpoint': 'LoanApplication?stage_id=1', 'source': 'Onboarding' }
+            this.https.fetchLoanApplicationList(data).subscribe(res => {
+                  this.loanApplicationData = res?.data?.results;
+                  this.total_count = res?.data?.total_count;
+                  this.api_calling_loader = false
+            }, (err) => {
+                  this.api_calling_loader = false
+            })
+            this.api_calling_loader = false
+      }
 
-  expandSet = new Set<number>();
-  onExpandChange(id: number, checked: boolean, index?): void {
-    if (checked) {
-      this.expandSet.add(id);
-      this.getIdWiseData(this._currentId = id, index);
-      // console.log();
-      
-    } else {
-      this.expandSet.delete(id);
-      console.log('Deleted array of active ids', this._activeLoans);
-    }
-  }
 
-  updateCheckedSet(id: number, checked: boolean): void {
-    if (checked) {
-      this.setOfCheckedId.add(id);
-    } else {
-      this.setOfCheckedId.delete(id);
-    }
-  }
+      getIdWiseData(id?, index?) {
+            let data = { 'datapoint': 'loan_application', 'endpoint': 'LoanApplication?id=' + id, 'source': 'Onboarding' };
+            this.https.fetchLoanApplicationList(data).subscribe(res => {
+                  this._activeLoans.push(res?.data?.results[0]);
+                  this.loanApplicationData[index].expanddata = res?.data?.results[0];
+                  console.log(this.loanApplicationData[index].expanddata)
+            })
+      }
 
-  onCurrentPageDataChange(listOfCurrentPageData: readonly Data[]): void {
-    this.listOfCurrentPageData = listOfCurrentPageData;
-    this.refreshCheckedStatus();
-  }
+      expandSet = new Set<number>();
+      onExpandChange(id: number, checked: boolean, index?): void {
+            if (checked) {
+                  this.expandSet.add(id);
+                  this.getIdWiseData(this._currentId = id, index);
+                  // console.log();
 
-  onItemChecked(id: number, checked: boolean): void {
-    this.updateCheckedSet(id, checked);
-    this.refreshCheckedStatus();
-  }
+            } else {
+                  this.expandSet.delete(id);
+                  console.log('Deleted array of active ids', this._activeLoans);
+            }
+      }
 
-  onAllChecked(checked: boolean): void {
-    this.listOfCurrentPageData
-      .filter(({ disabled }) => !disabled)
-      .forEach(({ id }) => this.updateCheckedSet(id, checked));
-    this.refreshCheckedStatus();
-  }
+      updateCheckedSet(id: number, checked: boolean): void {
+            if (checked) {
+                  this.setOfCheckedId.add(id);
+            } else {
+                  this.setOfCheckedId.delete(id);
+            }
+      }
 
-  refreshCheckedStatus(): void {
-    const listOfEnabledData = this.listOfCurrentPageData.filter(({ disabled }) => !disabled);
-    this.checked = listOfEnabledData.every(({ id }) => this.setOfCheckedId.has(id));
-    this.indeterminate = listOfEnabledData.some(({ id }) => this.setOfCheckedId.has(id)) && !this.checked;
-  }
+      onCurrentPageDataChange(listOfCurrentPageData: readonly Data[]): void {
+            this.listOfCurrentPageData = listOfCurrentPageData;
+            this.refreshCheckedStatus();
+      }
 
-  onMonthChange(event){
+      onItemChecked(id: number, checked: boolean): void {
+            this.updateCheckedSet(id, checked);
+            this.refreshCheckedStatus();
+      }
 
-  }
+      onAllChecked(checked: boolean): void {
+            this.listOfCurrentPageData
+                  .filter(({ disabled }) => !disabled)
+                  .forEach(({ id }) => this.updateCheckedSet(id, checked));
+            this.refreshCheckedStatus();
+      }
 
-  updateStatus(){
-    this._isUpdateStatus = true;
-  }
+      refreshCheckedStatus(): void {
+            const listOfEnabledData = this.listOfCurrentPageData.filter(({ disabled }) => !disabled);
+            this.checked = listOfEnabledData.every(({ id }) => this.setOfCheckedId.has(id));
+            this.indeterminate = listOfEnabledData.some(({ id }) => this.setOfCheckedId.has(id)) && !this.checked;
+      }
 
-  handleCancel(){
-    this._isUpdateStatus = false;
-  }
+      onMonthChange(event) {
 
-  handleOk(){
+      }
 
-  }
+      updateStatus() {
+            this._isUpdateStatus = true;
+            this.https.getStageMaster().subscribe(res => {
+                  if (res?.success) {
+                        this.stageMasterList = res?.data?.results
+                  }
+            })
+            console.log(this._checkedLoanList);
+      }
+
+      handleCancel() {
+            this._isUpdateStatus = false;
+      }
+
+      handleOk() {
+            let data = { source: 'Onboarding', datapoint: 'update_multi_application_status', stage_id: '1', applications: this._checkedLoanList };
+            this.https.updateMultipleLoanApp(data).subscribe(res => {
+                  if (res.success) {
+                        console.log('res');
+                        this._isUpdateStatus = false;
+                  } else {
+                        console.log('error=>', res?.error);
+                  }
+            }, error => {
+                  console.log(error);
+
+            })
+      }
+
+      checkDisabledStatus() {
+            this._checkedLoanList = Array.from(this.setOfCheckedId);
+            if (this._checkedLoanList.length > 0) {
+                  return false
+            } else {
+                  return true
+            }
+      }
 }
