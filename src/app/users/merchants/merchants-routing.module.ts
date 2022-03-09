e6ba75face31037bad9b8f46a4252c9ed5f15612
karan <karan.g@codezen.in>
2022-03-09 20:05:15 +0530

    qa issues

--- src/app/users/merchants/merchants-routing.module.ts
+++ src/app/users/merchants/merchants-routing.module.ts
@@ -6,85 +6,83 @@ import { DsaListComponent } from './dsa-list/dsa-list.component';
 import { MerchantsListComponent } from './merchants-list/merchants-list.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    data:{
-      title: "Merchant",
-      parent: "Merchants",
-    },
-    children: [
-  {
-    path: "DSA",
-    component: DsaListComponent,
-    data: {
-      title: "Partner DSA",
-      parent: "Merchants",
-    },
-    children: [
       {
-        path: "add-dsa",
-        component: AddEditDsaComponent,
-        data: {
-          title: "Add DSA",
-          parent: "DSA List",
-          custom_url: "add-dsa",
-        },
+            path: '',
+           
+            children: [
+                  { 
+                        path: "DSA",
+                        component: DsaListComponent,
+                        data: {
+                              title: "Partner DSA",
+                              parent: "Merchants",
+                        },
+                        children: [
+                              {
+                                    path: "add-dsa",
+                                    component: AddEditDsaComponent,
+                                    data: {
+                                          title: "Add DSA",
+                                          parent: "DSA List",
+                                          custom_url: "add-dsa",
+                                    },
+                              },
+                              {
+                                    path: "edit-dsa",
+                                    component: AddEditDsaComponent,
+                                    data: {
+                                          title: "Edit DSA",
+                                          parent: "DSA List",
+                                          custom_url: "edit-dsa",
+                                    },
+                              },
+                        ],
+                  },
+                  {
+                        path: "list",
+                        component: MerchantsListComponent,
+                        data: {
+                              title: "Merchant List",
+                              parent: "Merchants",
+                              custom_url: "list",
+                        },
+                        children: [],
+                  },
+                  {
+                        path: "list",
+                        data: {
+                              title: 'Merchant List',
+                              parent:'Merchants',
+                              custom_url: "merchants/list",
+                        },
+                        children: [
+                              {
+                                    path: "add-merchant",
+                                    component: AddEditMerchantsComponent,
+                                    data: {
+                                          title: "Add Merchant",
+                                          parent: "Merchant",
+                                          custom_url: "add-merchant",
+                                    },
+                              },
+                              {
+                                    path: "edit-merchant",
+                                    component: AddEditMerchantsComponent,
+                                    data: {
+                                          title: "Edit Merchant",
+                                          parent: "Merchant",
+                                          custom_url: "add-merchant",
+                                    },
+                              },
+                        ],
+                  },
+            ]
       },
-      {
-        path: "edit-dsa",
-        component: AddEditDsaComponent,
-        data: {
-          title: "Edit DSA",
-          parent: "DSA List",
-          custom_url: "edit-dsa",
-        },
-      },
-    ],
-  },
-  {
-    path: "list",
-    component: MerchantsListComponent,
-    data: {
-      title: "Merchant List",
-      parent: "Merchants",
-      custom_url: "list",
-    },
-    children: [],
-  },
-  {
-    path: "list",
-    data: {
-      title: "Merchant List",
-      custom_url: "merchants/list",
-    },
-    children: [
-      {
-        path: "add-merchant",
-        component: AddEditMerchantsComponent,
-        data: {
-          title: "Add Merchant",
-          parent: "Merchant List",
-          custom_url: "add-merchant",
-        },
-      },
-      {
-        path: "edit-merchant",
-        component: AddEditMerchantsComponent,
-        data: {
-          title: "Edit Merchant",
-          parent: "Merchant",
-          custom_url: "add-merchant",
-        },
-      },
-    ],
-  },
-]
-  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+      imports: [RouterModule.forChild(routes)],
+      exports: [RouterModule]
 })
 export class MerchantsRoutingModule { }
 
