2aa3a16062e0d94b70b495727b1994db66ca692b
karan <karan.g@codezen.in>
2022-03-09 10:35:41 +0530

    document issues solved

--- src/app/applications/nbfc-approval/nbfc-approval.component.ts
+++ src/app/applications/nbfc-approval/nbfc-approval.component.ts
@@ -54,7 +54,22 @@ export class NbfcApprovalComponent implements OnInit {
          globalPageSize : any;
          productList : any = []
          stageStatusList : any = []
-   
+
+      // Modal Boolean Values
+      _isPullData: boolean = false;
+      _isOpenModal: boolean = false;
+      _currentFileName: any ;
+      fileList: any = [];
+      _isViewDocument: any;
+      _isDownload: boolean = false;
+      _isVerify: boolean = false;
+      _isUpload: boolean = false;
+      _currentModalData: any;
+      _currentLoanDetails: any;
+      verifyRemarks: any;
+      _isCibil: boolean = false
+      documentStatus = 1
+      
          constructor(public https: HttpService, public message: NzMessageService, public global : GlobalservicesService) { }
    
          ngOnInit(): void {
@@ -79,7 +94,7 @@ export class NbfcApprovalComponent implements OnInit {
          getFormLoanData(tableFilter?) {
                this.api_calling_loader['listLoader'] = true
                this.loanApplicationData = [];
-               var data = { 'datapoint': 'loan_application', 'endpoint': 'LoanApplication?stage_id=1', 'source': 'Onboarding' }
+               var data = { 'datapoint': 'loan_application', 'endpoint': 'LoanApplication?stage_id=10', 'source': 'Onboarding' }
    
                if (this.filters) {
                      data['status'] = this.filters
@@ -203,7 +218,13 @@ export class NbfcApprovalComponent implements OnInit {
       }
 
       handleCancel() {
-            // this.remarks = '';
+           
+            this._isOpenModal = false;
+            this._isViewDocument = false;
+            this._isUpload = false;
+            this._isVerify = false;
+            this._isPullData = false;
+            this._isCibil = false;
             this._isUpdateStatus = false;
             this._isStatus = false;
             this._isDocument = false;
@@ -247,11 +268,42 @@ export class NbfcApprovalComponent implements OnInit {
                               this.message.error(res?.message)
                         }
                   })
+            }else if('uploadDocument'){
+                  this.api_calling_loader['button'] = true
+                        let uploadDoc = { source: 'Onboarding', datapoint: 'upload_kyc_doc', 'application_id': this._currentModalData['application'], 'kyc_document_id': this._currentModalData?.id, 'file': this._currentFileName }
+                        console.log(uploadDoc, 'For Upload Document');
+
+                        this.https.uploadLoanDocument(uploadDoc).subscribe((res : any)=>{
+                              if(res?.success){
+                                    this.api_calling_loader['button'] = false;
+                                    this.fileList = [];
+						this.message.success(res?.message)
+                                    this.handleCancel();
+                              }else{
+                                    this.api_calling_loader['button'] = false;
+                                    this.fileList = [];
+						this.message.error(res?.message)
+                                    this.handleCancel();
+                              }
+                        },err=>{this.api_calling_loader['button'] = false;
+				this.message.error(err)})
+            }else if('verify'){
+                  this.api_calling_loader['button'] = true
+                        let params = { source: 'Onboarding', datapoint: 'verify_kyc_doc', 'application_id': this._currentModalData['application'], 'kyc_document_id': this._currentModalData?.id, 'status': (this.documentStatus == 1 ? 'Accepted' : 'Rejected'), 'reason': this.verifyRemarks }
+                        console.log('export this file', this._currentModalData, params);
+				this.https.verifyLoanDocument(params).subscribe((res :any)=>{
+					if(res?.success){
+						this.api_calling_loader['button'] = false
+						this.message.success(res?.message);
+						this.handleCancel();
+						this.getIdWiseData(this._currentModalData['application'])
+					}else{
+						this.api_calling_loader['button'] = false
+						this.message.error(res?.message);
+					}
+				},err =>{this.api_calling_loader['button'] = false
+				this.message.error(err);})
             }
-      }
-
-      downloadModal() {
-
       }
 
       checkDisabledStatus() {
@@ -309,5 +361,39 @@ export class NbfcApprovalComponent implements OnInit {
             } else if (type == 'reject') {
                   this.isRejectOffer = true;
              }
+      }
+
+
+      openDocumentModal(type?, data?, loanData?) {
+		this._currentModalData = data;
+		this._currentLoanDetails = loanData;
+		if(type == 'download'){
+			let data = { source: 'Onboarding', datapoint: 'download_document','endpoint':'kyc', 'id': this._currentModalData?.id}
+			console.log(data);
+			this.https.downloadDocuments(data).subscribe((res:any)=>{
+if(res?.success){
+	// let url = window.URL.createObjectURL(blob)
+	let pwa = window.open(res?.file);
+}
+			});
+		}else{
+			this._isOpenModal = true;
+                  this._isUpdateStatus = true
+			console.log( this._currentModalData);
+			switch (type) {
+				case 'viewDocument': this._isViewDocument = true;
+					// this.generateBase64View(this._currentModalData?.file);
+					break;
+				case 'verify': this._isVerify = true; break;
+				case 'upload': this._isUpload = true; break;
+			}
+		}  
+      }
+
+      resetFilters(){
+            this.productFilters = null;
+            this.filters = null;
+            this.searchValue = null;
+            this.getFormLoanData()
       }
 }
