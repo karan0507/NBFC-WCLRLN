d84def807481bd44fc7a7d3984756f80aa00d7c4
shodowTech <theshield2229@gmail.com>
2022-04-22 12:18:55 +0530

    permission

--- src/app/lms/change-bill-date/app-side-change-bill-date/app-side-change-bill-date.component.ts
+++ src/app/lms/change-bill-date/app-side-change-bill-date/app-side-change-bill-date.component.ts
@@ -17,6 +17,18 @@ export class AppSideChangeBillDateComponent implements OnInit {
   globalPageSize = 30
   api_calling_loader: boolean;
   listOfData: any;
+  customRanges = {
+    Today: [new Date(), new Date()],
+    'Last 7 days': [new Date().setDate(new Date().getDate() - 7), new Date()],
+    'This Month': [new Date(new Date().getFullYear(), new Date().getMonth(), 1), new Date()],
+    'Last Month': [new Date(new Date().getFullYear(), new Date().getMonth(), 1).setMonth(new Date().getMonth() - 1), new Date(new Date().getFullYear(), new Date().getMonth(), -1)],
+    'Last 3 Months': [new Date(new Date().getFullYear(), new Date().getMonth(), 1).setMonth(new Date().getMonth() - 3), new Date()],
+    'Last 6 Months': [new Date(new Date().getFullYear(), new Date().getMonth(), 1).setMonth(new Date().getMonth() - 6)],
+    'This Year': [new Date(new Date().getFullYear(), 0, 1), new Date()],
+    // 'Last Year': [new Date(new Date().getFullYear(), new Date().getMonth(), 1).setMonth(new Date().getMonth() - 12), new Date(new Date().getFullYear(), new Date().getMonth(), 1)],
+    'Last Year': [new Date(new Date().getFullYear() - 1, new Date().getMonth(), 0).setDate(1), new Date(new Date().getFullYear() - 1, 11, 31)],
+    // d.setMonth(d.getMonth() - 3);
+};
   isReject: boolean;
   reject_id: any;
   rejactRemarks;
