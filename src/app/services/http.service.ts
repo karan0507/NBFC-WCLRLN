beab57e3f31d65468964d7fa762c81587265ebb2
shodowTech <theshield2229@gmail.com>
2022-05-11 17:29:36 +0530

    Qa ise

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -544,6 +544,11 @@ export class HttpService {
             return this._http.post((this.url + `/central-api/v1/call-api`), data);
       }
 
+      // Send E-mandate Link 
+      public sendEmandateLink(data): any {
+            return this._http.post((this.url + `/central-api/v1/call-api`), data);
+      }
+
       // Download Loan Application Documents 
       public downloadDocuments(data): any {
             return this._http.get((this.url + `/central-api/v1/call-api`), { params: data });
