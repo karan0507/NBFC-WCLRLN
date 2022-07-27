1b6679b513edd463106954deb266415fb4db6e4f
shodowTech <theshield2229@gmail.com>
2022-07-27 17:12:46 +0530

    sheet point

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -152,4 +152,22 @@ expandSet = new Set<number>();
       this.message.remove(generateloader);
     })
   }
+
+  public convertBillDateToText(date) {
+    var finalDate = ''
+    if (date) {
+      if (date[0] == 1) {
+        finalDate = date + 'st every month'
+      } else if (date[0] == 2) {
+        finalDate = date + 'nd every month'
+      } else if (date[0] == 3) {
+        finalDate = date + 'rd every month'
+      } else { 
+        finalDate = date + 'th every month'
+      }
+    } else {
+      finalDate = '-'
+    }
+    return finalDate
+  }
 }
