aa0bdf9ac525e077033daff139331b0c41fa9dd7
Gangaram Pednekar <gangaram.pednekar@codezen.in>
2022-11-14 16:48:08 +0530

    qa issues

--- src/app/lms/mandate-triggers/mandate-triggers.component.ts
+++ src/app/lms/mandate-triggers/mandate-triggers.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Data } from '@angular/router';
+import { differenceInCalendarDays } from 'date-fns';
+import * as moment from 'moment';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { HttpService } from 'src/app/services/http.service';
 
@@ -27,7 +29,23 @@ export class MandateTriggersComponent implements OnInit {
   corporateList: any[];
   amount: any;
   isCaptureCollection: boolean;
+  date = '';
+  disabledDate = (current: Date): boolean =>
+  // Can not select days before today and today
+  differenceInCalendarDays(current, new Date()) > 0;
 
+customRanges = {
+  Today: [new Date(), new Date()],
+  'Last 7 days': [new Date().setDate(new Date().getDate() - 7), new Date()],
+  'This Month': [new Date(new Date().getFullYear(), new Date().getMonth(), 1), new Date()],
+  'Last Month': [new Date(new Date().getFullYear(), new Date().getMonth(), 1).setMonth(new Date().getMonth() - 1), new Date(new Date().getFullYear(), new Date().getMonth(), -1, 30, 31)],
+  'Last 3 Months': [new Date(new Date().getFullYear(), new Date().getMonth(), 1).setMonth(new Date().getMonth() - 3), new Date(new Date().getFullYear(), new Date().getMonth(), -1, 30, 31)],
+  'Last 6 Months': [new Date(new Date().getFullYear(), new Date().getMonth(), 1).setMonth(new Date().getMonth() - 6), new Date(new Date().getFullYear(), new Date().getMonth(), -1, 30, 31)],
+  'This Year': [new Date(new Date().getFullYear(), 0, 1), new Date()],
+  // 'Last Year': [new Date(new Date().getFullYear(), new Date().getMonth(), 1).setMonth(new Date().getMonth() - 12), new Date(new Date().getFullYear(), new Date().getMonth(), 1)],
+  'Last Year': [new Date(new Date().getFullYear() - 1, 0, 1), new Date(new Date().getFullYear() - 1, 11, 31)],
+  // d.setMonth(d.getMonth() - 3);
+};
   constructor(public http: HttpService, private message: NzMessageService,
     private router: Router,
     private route: ActivatedRoute) { }
@@ -50,8 +68,8 @@ export class MandateTriggersComponent implements OnInit {
         // product_type: this.master_product_id ? this.master_product_id : '',
         // txn_status: this.selectedStatus ? this.selectedStatus : '',
         // txn_type: this.selectedType ? this.selectedType : '',
-        // start_date: this.date[0] ? moment(this.date[0]).format("YYYY-MM-DD") : '',
-        // end_date: this.date[1] ? moment(this.date[1]).format("YYYY-MM-DD") : '',
+        start_date: this.date[0] ? moment(this.date[0]).format("YYYY-MM-DD") : '',
+        end_date: this.date[1] ? moment(this.date[1]).format("YYYY-MM-DD") : '',
         keyword: this.searchValue,
         corporate: this.selectedCorporate ? this.selectedCorporate : '',
         // section: this.selectedTab
@@ -68,8 +86,8 @@ export class MandateTriggersComponent implements OnInit {
         // product_type: this.master_product_id ? this.master_product_id : '',
         // txn_status: this.selectedStatus ? this.selectedStatus : '',
         // txn_type: this.selectedType ? this.selectedType : '',
-        // start_date: this.date[0] ? moment(this.date[0]).format("YYYY-MM-DD") : '',
-        // end_date: this.date[1] ? moment(this.date[1]).format("YYYY-MM-DD") : '',
+        start_date: this.date[0] ? moment(this.date[0]).format("YYYY-MM-DD") : '',
+        end_date: this.date[1] ? moment(this.date[1]).format("YYYY-MM-DD") : '',
         keyword: this.searchValue,
         corporate: this.selectedCorporate ? this.selectedCorporate : '',
         section: this.selectedTab
@@ -172,7 +190,7 @@ export class MandateTriggersComponent implements OnInit {
     this.searchValue = ''
     // this.selectedType = ''
     // this.selectedStatus = ''
-    // this.date = ''
+    this.date = ''
     this.selectedCorporate = ''
     this.fetchMandateTriggerList();
   }
