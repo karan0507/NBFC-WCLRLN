719f1906468fb363c0c9c0b5187b51df414e5e56
Gangaram Pednekar <gangaram.pednekar@codezen.in>
2023-01-19 17:47:47 +0530

    qa

--- src/app/products/create-product/create-product.module.ts
+++ src/app/products/create-product/create-product.module.ts
@@ -23,6 +23,7 @@ import { NgxPermissionsModule } from 'ngx-permissions';
 import { NzTagModule } from 'ng-zorro-antd/tag';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { TransactionConfigComponent } from './transaction-config/transaction-config.component';
+import { LmhComponent } from './lmh/lmh.component';
 
 
 @NgModule({
@@ -37,7 +38,7 @@ import { TransactionConfigComponent } from './transaction-config/transaction-con
     UnderwritingComponent, 
     OnboardingComponent, 
     FeesComponent, 
-    LoanAgreementComponent, ViewProductComponent, TransactionConfigComponent],
+    LoanAgreementComponent, ViewProductComponent, TransactionConfigComponent, LmhComponent],
   imports: [
     CommonModule,
     CreateProductRoutingModule,
