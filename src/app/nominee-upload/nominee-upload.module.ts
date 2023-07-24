e1a7e4076522852057f74db4a5063fcc2036986a
Gangaram Pednekar <gangaram.pednekar@codezen.in>
2023-07-24 13:22:38 +0530

    build for prod  server

--- src/app/nominee-upload/nominee-upload.module.ts
+++ src/app/nominee-upload/nominee-upload.module.ts
@@ -4,12 +4,17 @@ import { CommonModule } from '@angular/common';
 import { NomineeUploadRoutingModule } from './nominee-upload-routing.module';
 import { BulkNomineeUploadComponent } from './bulk-nominee-upload/bulk-nominee-upload.component';
 import { DemoNgZorroAntdModule } from '../ng-zorro-antd.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [BulkNomineeUploadComponent],
   imports: [
     CommonModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     NomineeUploadRoutingModule,
     DemoNgZorroAntdModule
   ]
